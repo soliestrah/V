8dada1be1ab708f2073bfe4ee74252a87fb67551
solie <solene.hernando@gmail.com>
2026-05-15 09:48:33 +0200

    Fix navigation list items and add 'charte' link

--- scripts/nav.js
+++ scripts/nav.js
@@ -20,8 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
           <li class="nav__item"><a href="medecin.html"><span class="nav__index">05</span>medecin</a></li>
           <li class="nav__item"><a href="astral.html"><span class="nav__index">06</span>astrologie</a></li>
           <li class="nav__item"><a href="wardrobe.html"><span class="nav__index">07</span>wardrobe</a></li></ul>
-      <li class="nav__item"><a href="wishlist.html"><span class="nav__index">08</span>wishlist</a></li></ul>
-     <li class="nav__item"><a href="wardrobecpasule.html"><span class="nav__index">09</span>la capsule</a></li></ul> 
+          <li class="nav__item"><a href="wishlist.html"><span class="nav__index">08</span>wishlist</a></li></ul>
+          <li class="nav__item"><a href="wardrobecpasule.html"><span class="nav__index">09</span>la capsule</a></li></ul> 
+          <li class="nav__item"><a href="charte.html"><span class="nav__index">10</span>charte</a></li></ul>
        </nav>
 
       <footer class="sidebar__footer">
